52a9bc56e454a29c18ca69a65fcb2cb5f9532ee0
unknown <luuis.avelino@gmail.com>
2022-06-10 01:08:49 -0300

    Adicionado os primeiros arquivos

---
+++ App/microcontrolers/publisher/publisher.ino
@@ -0,0 +1,52 @@
+#include "EspMQTTClient.h"
+
+/************************* WiFi Access Point *********************************/
+
+#define WLAN_SSID       "PRISMA"
+#define WLAN_PASS       "12082000"
+
+/***************************** Broker MQTT ***********************************/
+
+#define SERVER_IP      "192.168.1.122"
+#define SERVER_PORT     1883
+#define IDENTIFY_NAME   "TestClient"
+
+/*************************** Network Config **********************************/
+
+EspMQTTClient client(
+  WLAN_SSID,
+  WLAN_PASS,
+  SERVER_IP,
+  IDENTIFY_NAME,
+  SERVER_PORT
+);
+
+/*************************** Variables **********************************/
+
+const int analogInPin = A0;  // ESP8266 Analog Pin ADC0 = A0
+char sensorValue[20];  // value read from the pot
+
+/*************************** Code **********************************/
+
+void setup()
+{
+  Serial.begin(115200);
+
+  client.enableDebuggingMessages();     // Enable debugging messages sent to serial output
+  client.enableHTTPWebUpdater();        // Enable the web updater. User and password default to values of MQTTUsername and MQTTPassword. These can be overridded with enableHTTPWebUpdater("user", "password").
+  client.enableLastWillMessage("TestClient/lastwill", "I am going offline", true);      // You can activate the retain flag by setting the third parameter to true
+}
+
+void onConnectionEstablished()
+{
+  Serial.println("Connected...");
+}
+
+void loop()
+{
+  itoa(analogRead(analogInPin), sensorValue, 10);
+
+  client.loop();
+  client.publish("sensor", sensorValue, true);
+  delay(0.01 * 1000);
+}
---
+++ App/microcontrolers/subscriber/subscriber.ino
@@ -0,0 +1,53 @@
+#include "EspMQTTClient.h"
+
+/************************* WiFi Access Point *********************************/
+
+#define WLAN_SSID       "PRISMA"
+#define WLAN_PASS       "12082000"
+
+/***************************** Broker MQTT ***********************************/
+
+#define SERVER_IP       "192.168.1.122"
+#define SERVER_PORT     1883
+#define IDENTIFY_NAME   "TestClient"
+
+/*************************** Network Config **********************************/
+
+EspMQTTClient client(
+  WLAN_SSID,
+  WLAN_PASS,
+  SERVER_IP,
+  IDENTIFY_NAME,
+  SERVER_PORT
+);
+
+/*************************** Variables **********************************/
+
+const int ledPin = 2;
+int dutyCycle = 0;
+
+/*************************** Code **********************************/
+
+void setup()
+{
+  Serial.begin(115200);
+
+  client.enableDebuggingMessages();     // Enable debugging messages sent to serial output
+  client.enableHTTPWebUpdater();        // Enable the web updater. User and password default to values of MQTTUsername and MQTTPassword. These can be overridded with enableHTTPWebUpdater("user", "password").
+  client.enableLastWillMessage("TestClient/lastwill", "I am going offline", true);      // You can activate the retain flag by setting the third parameter to true
+}
+
+void onConnectionEstablished()
+{
+  Serial.println("Connected...");
+  client.subscribe("sensor", [](const String & payload) {
+    dutyCycle = ((atoi(payload.c_str()) / 4) - 256) * (-1);
+    analogWrite(ledPin, dutyCycle);
+  }); 
+}
+
+void loop()
+{
+  client.loop();
+  delay(0.01 * 1000);
+}
