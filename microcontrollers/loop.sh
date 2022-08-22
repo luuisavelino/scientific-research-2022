5e447baa5c36afebf4c082b3fd4ffd967a577de3
Luis Avelino <luuis.avelino@gmail.com>
2022-08-22 09:22:42 -0300

    Added resources request to influxdb

---
+++ microcontrollers/loop.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+i=0
+while [ $i -ne 1000 ]
+do
+        i=$(($i+1))
+
+	mosquitto_pub -h 34.171.228.78 -t "sensor/horario" -m "$(date +%s%N)";
+        
+	#echo "$i - $(date +%s%n)"
+done
--- microcontrollers/teste.sh
+++
@@ -1,6 +0,0 @@
-while : ; do
-    date +%s%N
-    mosquitto_pub -h 34.172.56.210 -t "sensor/horario" -m "$(date +%s%N)";   
-
-    sleep 0.01;
-done
